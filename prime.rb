53cd1da3fb25e449452d5c945b8110e5595c0f3e
Mike Shahindoust <gobias101@gmail.com>
2020-08-05 22:37:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 def prime?()
   if num < 0 || num == 0 || num == 1
     return false
-  elsif
+  else
     (2..num - 1).to_a.all? do |factoring|
       num % factoring != 0
-      return true
     end
   end
 end
